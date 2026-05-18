d7fa5c7c7434f37aed94d1c022b28f32736f39f5
Fivigati Nurhayati <fivigati@gmail.com>
2026-05-18 17:16:34 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbz4NLt_x5yzbgZEE82uIqW3VS9Sv3yuNVeyqJ53SUpLuShTVucRmZLLo4B8HL0jaXrpug/exec';
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyppJyo4jYIpsa4GFrG7QKD8TrK6S7DRiAxLWVY3mtzUUOAQOEIk3hZRwEu9QLx1Bm7eQ/exec';
     window.correctToken = "";
     window.examLink = "";
     window.currentZoom = 100;
