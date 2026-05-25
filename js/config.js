4cbaba07cb2ce4c0af456eac442b0d83f79b1064
Fivigati Nurhayati <fivigati@gmail.com>
2026-05-25 21:23:55 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,19 +1,4 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzLGEOK7omDS-5TNMeFOfFmIWmTNwwO-CzrdP1sBvONZ5J8yRzJXSMpwyYvEMr0JVZV6g/exec';
-    window.correctToken = "";
-    window.examLink = "";
-    window.currentZoom = 100;
-    window.timerInterval = null;
-    window.isExamActive = false;
-    window.isForceClosing = false;
-    
-    // --- State Baru untuk Security ---
-    window.sessionData = {
-    npsn: "",
-    nisn: "",
-    exam_id: "",
-    exit_token: "",
-    userIP: "0.0.0.0",
-    wakeLock: null,
-    minExitMinutes: 10
+// Ganti string di bawah ini dengan URL Web App hasil deployment terbaru dari GAS Anda
+const CONFIG = {
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbz2dWa9GGHKWuTbTncB-MrXOUcnfNFc7mjWiZnhbyuuAx1RN-2Mz3u-Zaa3Dghn1WLwnw/exec"
 };
-    const isIOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
